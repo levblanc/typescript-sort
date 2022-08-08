8dbc574f65721d4c2c4cd3a7786d659e53595d16
levblanc <levblanc@gmail.com>
2022-08-08 17:01:45 +0800

    Add `CharactersCollection` class for string sorting

--- build/index.js
+++ build/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const collection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(collection);
+const CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+// const sorter = new Sorter(numbersCollection);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(collection.data);
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const collection = new NumbersCollection([10, 3, -5, 0])
-const sorter = new Sorter(collection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+// const sorter = new Sorter(numbersCollection);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(collection.data);
+console.log(charactersCollection.data);
